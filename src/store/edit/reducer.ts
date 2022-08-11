2b770138e820c770b9a839c7f3e1a6adc2ae30c1
AlmirQueiroga <almirqueiroga@gmail.com>
2022-08-10 22:11:50 -0300

    fixes

--- src/store/edit/reducer.ts
+++ src/store/edit/reducer.ts
@@ -5,10 +5,8 @@ const initialState: actionTypes.EditarState = {
 }
 
 const reducer = (state: actionTypes.EditarState = initialState, action: actionTypes.EditarAction): actionTypes.EditarState => {
-  console.log('asd')
   switch (action.type) {
     case actionTypes.ADD_OBJETO:
-      console.log('olha aqui chegou', action.value)
       return { editar: action.value }
     case actionTypes.REMOVE_OBJETO:
       return { editar: undefined }
